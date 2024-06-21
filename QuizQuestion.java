955944d173c0beaca1705b3ec58f2b839cb0cf3f
TheMaskdon <149241481+TheMaskdon@users.noreply.github.com>
2024-06-21 21:35:06 +0530

    Create QuizQuestion.java

---
+++ QuizQuestion.java
@@ -0,0 +1,23 @@
+public class QuizQuestion {
+    private String question;
+    private String[] options;
+    private char correctAnswer;
+
+    public QuizQuestion(String question, String[] options, char correctAnswer) {
+        this.question = question;
+        this.options = options;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String[] getOptions() {
+        return options;
+    }
+
+    public char getCorrectAnswer() {
+        return correctAnswer;
+    }
+}
